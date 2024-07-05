8feb5b3294a20eb863ca5e6c64d9a384a0285bfa
rajan <rajansuhagiya1234g@gmail.com>
2024-07-05 19:40:04 +0530

    add login, registration, task component with css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import TaskDashboard from "./components/TaskDashboard/TaskDashboard";
 import Register from "./components/Auth/Register/Register";
 import Login from "./components/Auth/Login/Login";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 function App() {
   return (
     <>
